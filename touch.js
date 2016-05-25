adc1808e792627e6cedb482a21ae9dc71d9f7bfe
JMartinUSA <waverider.jmart@gmail.com>
2016-05-25 17:56:35 -0400

    Update touch.js

--- touch.js
+++ touch.js
@@ -35,19 +35,19 @@ menuCont.on("swiperight", function(ev) {
     console.log('You swiped right on "menuCont"');
 });
 
-var messagesCont = document.getElementById('messagesCont');
+// var messagesCont = document.getElementById('messagesCont');
 
-// We create a manager object, which is the same as Hammer(), but without the presetted recognizers. 
-messagesCont = new Hammer.Manager(messagesCont);
+// // We create a manager object, which is the same as Hammer(), but without the presetted recognizers. 
+// messagesCont = new Hammer.Manager(messagesCont);
 
-messagesCont.add( new Hammer.Swipe);
-// This is where the magic happens
-messagesCont.on("swipeleft", function(ev) {
-    console.log('You swiped left on "messagesCont"');
-});
-messagesCont.on("swiperight", function(ev) {
-    console.log('You swiped right on "messagesCont"');
-    document.getElementById('messagesCont').innerHTML = '';
-});
+// messagesCont.add( new Hammer.Swipe);
+// // This is where the magic happens
+// messagesCont.on("swipeleft", function(ev) {
+//     console.log('You swiped left on "messagesCont"');
+// });
+// messagesCont.on("swiperight", function(ev) {
+//     console.log('You swiped right on "messagesCont"');
+//     document.getElementById('messagesCont').innerHTML = '';
+// });
 
 
