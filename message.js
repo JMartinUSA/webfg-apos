73adba8fdcff9e57aef4e86f93d45344b21fb8ee
JMartinUSA <waverider.jmart@gmail.com>
2016-05-25 15:02:37 -0400

    Add files via upload

--- message.js
+++ message.js
@@ -39,5 +39,5 @@ function pullMessages(){
 		<div class="messageName">Jesse</div>\
 		<div class="messageDate">Friday, 1:41 PM</div>\
 		<div class="messageText">This is the text of the message hello this. thajfla asjfla fa dflja sdflj adsfj aslfj as</div>\
-	</div>';
+	</div>';	
 }
